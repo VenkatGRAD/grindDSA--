60a30e3b3043fd3410371d40d83f5e6464467d06
VENKAT PRASAD <avprasad2002@gmail.com>
2025-06-24 13:46:15 +0530

    bubble Sort optimised code also

--- sorting/bubbleSort.java
+++ sorting/bubbleSort.java
@@ -17,10 +17,41 @@ public class bubbleSort {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
         }
+        System.out.println("");
         
     }
+
+    public static int bubbleSortOpti(int arr[]){
+        for (int i = 0; i < arr.length-1; i++) {
+                int swaps = 0; 
+                for (int j = 0; j < arr.length-1-i; j++) {
+                        if(arr[j]> arr[j+1]){
+                            int temp = arr[j];
+                            arr[j]=arr[j+1];
+                            arr[j+1]=temp;
+                            swaps++;
+                        }
+                }
+                if (swaps == 0) {
+                    return 1;
+                }
+        }
+
+        return -1;
+
+        
+    }
+
     public static void main(String[] args) {
-        int arr[]={5,4,1,3,2};
+        int arr[] = {2,3,6,8,10};
         bubbleSort(arr);
+        int ans = bubbleSortOpti(arr);
+        if (ans == 1) {
+            System.out.println("Array is already sorted");
+        }else{
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i]);
+            }
+        }
     }
 }
